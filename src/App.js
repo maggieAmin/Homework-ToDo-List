71fa9f0f2e23d0c18fd36ac0d3fb78a3009ac2fd
Maggie Amin <maggiemmagdy@gmail>
2023-01-16 23:56:42 +0000

    finished the mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Buy shopping" },
+    { id: 2, name: "Clean bathroom" },
+    { id: 3, name: "Car's MOT" }
+  ])
+
+  const [newTask, setNewTask] = useState("")
+
+  const handleTaskInput = (evt) => {
+    setNewTask(evt.target.value)
+  }
+
+  const saveNewTask = (evt) => {
+    evt.preventDefault()
+    const newTaskobj = {id: Date.now(), name: newTask}
+    const newToDoList = [...tasks, newTaskobj]
+    setTasks(newToDoList)
+    setNewTask("")
+  }
+
+  const completeTask = (id) => {
+    const newTasks = tasks.filter((task) => task.id !== id)
+    setTasks(newTasks)
+  }
+
+  const toDoTasks = tasks.map((task) => {
+    return (
+      <li key={task.id}>
+        {task.name}
+      <button onClick={() => {completeTask(task.id)}}>Complete</button>
+      </li>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <hr></hr>
+      <ul>
+        {toDoTasks}
+      </ul>
+
+      <form onSubmit={saveNewTask}>
+        <label htmlFor='new-task'>Task name</label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput}></input>
+        <input type="submit" value="Save new Task"></input>
+      </form>
+      
     </div>
   );
 }
